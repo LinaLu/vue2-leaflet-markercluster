29775cd97cbce035b31130f062068e2310c6915e
Lina Lu <lina.my.lu@gmail.com>
2019-06-22 17:06:04 +0200

    Update Vue2LeafletMarkercluster.vue

--- Vue2LeafletMarkercluster.vue
+++ Vue2LeafletMarkercluster.vue
@@ -5,46 +5,64 @@
 </template>
 
 <script>
-import { MarkerClusterGroup } from 'leaflet.markercluster'
-import { findRealParent, propsBinder } from 'vue2-leaflet'
-import { DomEvent } from 'leaflet'
-
-const props = {
-  options: {
-    type: Object,
-    default() { return {}; },
-  },
-};
+import { findRealParent, propsBinder, L } from 'vue2-leaflet';
+import 'leaflet.markercluster';
 
 export default {
-  props,
-  data() {
+  props: {
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+    total: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
     return {
-      ready: false,
+      ready: false
     };
   },
-  mounted() {
-    this.mapObject = new MarkerClusterGroup(this.options);
-    DomEvent.on(this.mapObject, this.$listeners);
-    propsBinder(this, this.mapObject, props);
-    this.ready = true;
+  mounted () {
+    this.mapObject = L.markerClusterGroup(this.options);
+    L.DomEvent.on(this.mapObject, this.$listeners);
+    propsBinder(this, this.mapObject, this.$options.props);
     this.parentContainer = findRealParent(this.$parent);
-    this.parentContainer.addLayer(this);
+    this._layerAdded = 0;
+    this._appended = false;
+    this.ready = true;
   },
-  beforeDestroy() {
-    this.parentContainer.removeLayer(this);
+  beforeDestroy () {
+    if (this._appended) {
+      this.parentContainer.removeLayer(this);
+    }
   },
   methods: {
-    addLayer(layer, alreadyAdded) {
+    addLayer (layer, alreadyAdded) {
       if (!alreadyAdded) {
         this.mapObject.addLayer(layer.mapObject);
+        this._layerAdded += 1;
+        this.appendToMap();
       }
     },
-    removeLayer(layer, alreadyRemoved) {
+    removeLayer (layer, alreadyRemoved) {
       if (!alreadyRemoved) {
         this.mapObject.removeLayer(layer.mapObject);
       }
     },
+    appendToMap () {
+      if (this._layerAdded >= this.total && !this._appended) {
+        this.parentContainer.addLayer(this);
+        this._appended = true;
+      }
+    }
   },
+  render: function (h) {
+    if (this.$slots.default && this.ready) {
+      return h('div', { style: { display: 'none' } }, this.$slots.default);
+    }
+    return null;
+  }
 };
 </script>
